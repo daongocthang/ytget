a1b8d47edb5317fdbe226b0f131fee1c0e4a18c4
wayfar3 <daongocthang.vn@gmail.com>
2021-05-16 08:27:22 +0700

    add helps

--- audicon.py
+++ audicon.py
@@ -3,20 +3,21 @@ import os
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-o', metavar='PATH', dest='output', default=None)
-    parser.add_argument('--format', default='mp3')
-    parser.add_argument('-y', dest='yes', action='store_true')
-    parser.add_argument('input')
+    parser.add_argument('-o', metavar='PATH', dest='output',
+                        help='Destination to save the audio file (default: the path of source file)')
+    parser.add_argument('--format', default='mp3', help='Type of audio file (default: mp3)')
+    parser.add_argument('-y', dest='yes', action='store_true',
+                        help='Permission to remove the source file after finished')
+    parser.add_argument('input', help='The source file need to convert')
 
     args = parser.parse_args()
     try:
-
+        output = args.output
         src = args.input
         f_name, f_ext = os.path.splitext(os.path.basename(src))
         fmt = args.format
 
-        path = args.output if args.output else os.path.dirname(os.path.abspath(src))
-        dest = os.path.join(path, '.'.join([f_name, fmt]))
+        dest = os.path.join(output if output else os.path.dirname(src), '.'.join([f_name, fmt]))
 
         print(f'[+] Converting {f_ext.lstrip(".")} to {fmt}')
 
